7e022915b1ec2b0f2b0695c1e0b8b6b4116e09a6
masashi.takada <b132036@gmail.com>
2021-12-30 02:57:39 +0900

    20211230_棒グラフの表示部分を追加

--- managedata/urls.py
+++ managedata/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name = 'managedata'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('api/v1/person-ls', views.PersonListAPIView.as_view(),
+         name="person_list_api"),
 ]
--- managedata/views.py
+++ managedata/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Person
+from rest_framework import generics
+from .serializer import PersonSerializer
 
 
 class IndexView(generic.ListView):
     model = Person
     template_name = "index.html"
+
+
+class PersonListAPIView(generics.ListAPIView):
+    queryset = Person.objects.all()
+    serializer_class = PersonSerializer
